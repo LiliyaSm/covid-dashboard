9e9c81e8ed173a243e09071138dcbed5355a8eab
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2020-12-09 01:47:26 +0300

    feat: install react, eslint, bootstrap

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-plusplus': 'off',
+    'no-continue': 'off',
+    'linebreak-style': ['error', 'windows'],
+    indent: ['error', 2],
+  },
+  plugins: ['react'],
+};
