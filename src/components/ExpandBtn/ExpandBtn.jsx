8c4d4b662373712b1613e161a415ea6027665282
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2020-12-12 21:59:43 +0300

    feat: add closing

---
+++ src/components/ExpandBtn/ExpandBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ExpandBtn.scss';
+
+const ToggleBtn = () => {
+  const handleToddleWidth = (event) => {
+    event.target.parentNode.classList.toggle('full-container');
+    event.target.classList.toggle('shrink-background');
+  };
+
+  return <button onClick={handleToddleWidth} aria-label="expand" type="button" className="expand-btn" />;
+};
+
+export default ToggleBtn;
