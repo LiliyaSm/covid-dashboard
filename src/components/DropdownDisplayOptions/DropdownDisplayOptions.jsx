3c4d4fb139dda6a4a2341d1918c7ce165797516d
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2020-12-23 20:33:32 +0300

    feat: remove resize buttons

--- src/components/DropdownDisplayOptions/DropdownDisplayOptions.jsx
+++ src/components/DropdownDisplayOptions/DropdownDisplayOptions.jsx
@@ -1,25 +1,16 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import React from 'react';
 import PropTypes from 'prop-types';
-// import * as constants from '../../data/constants';
-import useResize from '../../hooks/useResize';
 
 const DropdownDisplayOptions = ({ setCurrShowingData, options, selectedKey, title }) => {
   const handleSelect = (e) => {
     setCurrShowingData(e);
   };
 
-  const width = useResize();
-  const menuText = () => {
-    if (width > 1490) {
-      return selectedKey ? options[selectedKey] : title;
-    }
-    return '';
-  };
   return (
     <Dropdown onSelect={handleSelect}>
       <Dropdown.Toggle className="dropdown-toggle" key="info" variant="info" title={title}>
-        {menuText()}
+        {selectedKey ? options[selectedKey] : title}
       </Dropdown.Toggle>
       <Dropdown.Menu>
         {Object.keys(options).map((key) => (
